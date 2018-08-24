ac3123526a00ed1c8ffdb6239ebde5f61a314089
Kimberley Hayward <haywarkimb@ecs.vuw.ac.nz>
2018-08-25 11:49:33 +1200

    setup initial express backend

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var serveStatic = require('serve-static');
+var finalhandler = require('finalhandler');
+var server = serveStatic('static');
+
+app.get('/', function(req, res){
+    server(req, res, finalhandler(req, res));
+});
+
+// app.use(serveStatic('static'));
+
+app.listen(5000);
