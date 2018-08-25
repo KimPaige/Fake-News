0d29ce44f6acbe700edeec0721e0c9b8201bb1d2
Mathias Ronimus <mathiar225@gmail.com>
2018-08-26 10:13:24 +1200

    Add comment analysis using watson

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,95 @@
-var get = require('./spike-comments').get;
+var getComments = require('./spike-comments').get;
 var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
+var fs = require('fs');
+var path = require('path');
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
-//var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+//Get the secret key
+let keyPath = path.join(__dirname, '..', '..', '.key');
+fs.readFile(keyPath, 'utf8', (err, key) => {
 
-/*var toneAnalyzer = new ToneAnalyzerV3({
-    version: '{version}',
-    iam_apikey: '{iam_api_key}',
-    url: '{url}'
-  });*/
+    //Initialize the tone analyzer service
+    var toneAnalyzer = new ToneAnalyzerV3({
+        version: '2016-05-19',
+        iam_apikey: key,
+        url: 'https://gateway-syd.watsonplatform.net/tone-analyzer/api'
+    });
+
+    getComments(comments => {
+
+        let mostAnger = 0;
+        let mostAngerComment;
+
+        let mostDisgust = 0;
+        let mostDisgustComment;
+
+        let mostFear = 0;
+        let mostFearComment;
+
+        let mostJoy = 0;
+        let mostJoyComment;
+
+        let mostSad = 0;
+        let mostSadComment;
+
+        let asyncNumDone = 0;
+        let asyncNumToDo = comments.length;
+
+        comments.forEach(comment => {
+            //Analyze the tone of each comment, finding the highest of each category
+
+            var toneParams = {
+                'tone_input': { 'text': comment.text },
+                'content_type': 'application/json'
+            };
+
+            toneAnalyzer.tone(toneParams, function (error, toneAnalysis) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    var tonesArray = toneAnalysis.document_tone.tone_categories[0].tones;
+
+                    if (tonesArray[0].score > mostAnger) {
+                        mostAnger = tonesArray[0].score;
+                        mostAngerComment = comment;
+                    }
+
+                    if (tonesArray[1].score > mostDisgust) {
+                        mostDisgust = tonesArray[1].score;
+                        mostDisgustComment = comment;
+                    }
+
+                    if (tonesArray[2].score > mostFear) {
+                        mostFear = tonesArray[2].score;
+                        mostFearComment = comment;
+                    }
+
+                    if (tonesArray[3].score > mostJoy) {
+                        mostJoy = tonesArray[3].score;
+                        mostJoyComment = comment;
+                    }
+
+                    if (tonesArray[4].score > mostSad) {
+                        mostSad = tonesArray[4].score;
+                        mostSadComment = comment;
+                    }
+                }
+
+                asyncNumDone++;
+                if (asyncNumDone === asyncNumToDo) {
+                    //Add the comments of each category
+                    console.log(mostAngerComment);
+                    console.log(mostDisgustComment);
+                    console.log(mostFearComment);
+                    console.log(mostJoyComment);
+                    console.log(mostSadComment);
+                }
+            });
+        });
+    });
+});
 
 app.use(serveStatic('static/'));
 
