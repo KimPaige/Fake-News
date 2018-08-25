7b870be35f72f84c0d175bb0cef3651388270425
Mathias Ronimus <mathiar225@gmail.com>
2018-08-26 09:26:06 +1200

    Restructure files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,15 @@
+var get = require('./spike-comments').get;
 var express = require('express');
 var app = express();
 var serveStatic = require('serve-static');
 
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+//var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
-var toneAnalyzer = new ToneAnalyzerV3({
+/*var toneAnalyzer = new ToneAnalyzerV3({
     version: '{version}',
-    username: '{username}',
-    password: '{password}',
+    iam_apikey: '{iam_api_key}',
     url: '{url}'
-  });
+  });*/
 
 app.use(serveStatic('static/'));
 
